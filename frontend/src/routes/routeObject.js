66e1402473cbd9293c9769b4b57e63755c86c673
young <oyy_ying@hotmail.com>
2022-03-14 00:38:45 +0800

    delete custom auth pages

--- frontend/src/routes/routeObject.js
+++ frontend/src/routes/routeObject.js
@@ -3,8 +3,6 @@ export default {
   inventory: { id: 'inventory', url: '/store/items' },
   analytic: { id: 'analytic', url: '/analytic' },
   search: { id: 'search', url: '/search/items' },
-  login: { id: 'login', url: '/user/login' },
-  register: { id: 'register', url: '/user/register' },
   account: { id: 'account', url: '/user/account' },
   cart: { id: 'account', url: '/cart' }
 };
