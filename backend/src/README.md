70c72c731cc381ae91d4e46adb6799cf510069aa
marcustw <e0014884@u.nus.edu.sg>
2022-04-07 14:05:35 +0800

    fix: use DB HOST and DB PORT secret and setup readme

---
+++ backend/src/README.md
@@ -0,0 +1,8 @@
+# Save & Save Backend
+
+## Configurations
+1. Set up Postgres Amazon RDS Database
+2. Add a file `secrets.py` with required secret environment variables `USERNAME, PASSWORD, DATABASE, DB_HOST, DB_PORT`
+3. Zip each microservice code
+4. Create AWS Lambda for each microservice and upload the zipped code
+5. Configure your psycopg2 layer and 
--- backend/src/product/db.py
+++ backend/src/product/db.py
@@ -1,11 +1,11 @@
 import psycopg2
-from secrets import USERNAME, PASSWORD, DATABASE
+from secrets import USERNAME, PASSWORD, DATABASE, DB_HOST, DB_PORT
 
 def test_db():
     conn = psycopg2.connect(user=USERNAME,
                               password=PASSWORD,
-                              host="save.cdc2z2pnuvzu.us-east-1.rds.amazonaws.com",
-                              port="5432",
+                              host=DB_HOST,
+                              port=DB_PORT,
                               database=DATABASE)
     cursor = conn.cursor()
     return cursor, conn
